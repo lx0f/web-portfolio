b3718899246af3a9f551aba57740a619d6630dac
Work <luthandyka.business@gmail.com>
2023-03-27 13:44:03 +0800

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="card">
+    <h1>Luth Andyka</h1>
+    <p>Hi, I'm a full-stack developer from Singapore.</p>
+    <ul>
+      <li>
+        Phone number: <a href="https://wa.me/6592229652">+65 92229652</a> <small>(whatsapp)</small>
+      </li>
+      <li>
+        Email: <a href="mailto:luthandyka.business@gmail.com">luthandyka.business@gmail.com</a>
+      </li>
+      <li>Github: <a href="https://github.com/lx0f">lx0f</a></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+/* stylify this anchor tag */
+/* include sick animation on hover */
+a {
+  color: white;
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+}
+
+a:hover {
+  color: yellow;
+  font-size: 16em;
+  font-weight: 900;
+  transform: scale(1.1);
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  background-color: salmon;
+  color: white;
+  text-align: center;
+  margin: auto;
+  padding: 2rem;
+  border-radius: 1rem;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+h1 {
+  font-size: 2.5rem;
+  font-weight: 700;
+  margin-bottom: 0.5rem;
+}
+</style>
