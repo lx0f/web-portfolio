b5bcc232dc543ca459c644a62381df21bd4205e7
Work <luthandyka.business@gmail.com>
2023-03-29 14:36:26 +0800

    add audio autoplay

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,32 @@
 <script setup lang="ts">
+import HelloCard from "./components/HelloCard.vue";
 import HappyCard from "./components/HappyCard.vue";
 import SadCard from "./components/SadCard.vue";
 
 import { ref } from "vue";
 
 const isHappy = ref(true);
+const isStart = ref(false);
 
-function simulateClick() {
-  const button = document.getElementById("button");
-  const event = new MouseEvent('click', {
-    view: window,
-    bubbles: true,
-    cancelable: true
-  });
-  button?.dispatchEvent(event);
-}
-simulateClick();
-
-setTimeout(() => {
+function triggerHorror() {
+  isStart.value = true;
+  setTimeout(() => {
   document.getElementsByTagName("html")[0].style.backgroundImage = "url('/mugshot-mod.png')";
   document.getElementsByTagName("html")[0].style.backgroundColor = "black";
   isHappy.value = false;
+  document.getElementsByTagName("audio")[0].play();
   alert("It's in the walls, it's in the walls, it's in the walls, it's in the walls");
   alert("look behind you");
 }, 3000);
+}
 </script>
 
 <template>
-  <component :is="isHappy ? HappyCard : SadCard" />
+  <HelloCard v-if="!isStart" >
+    <button id="button" @click="triggerHorror();">Start</button>
+  </HelloCard>
+  <component v-if="isStart" :is="isHappy ? HappyCard : SadCard" />
+  <audio id="audio" src="/ambience.mp3"></audio>
 </template>
 
 <style scoped>
---
+++ src/components/HelloCard.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+
+</script>
+<template>
+<div class="card">
+    <h1>Hello!</h1>
+    <p>Thank you for coming to my website.</p>
+    <p>I not speak english well but I appreciate your coming 🥰🥰🥰</p>
+    <slot></slot>
+</div>
+</template>
+<style scoped>
+a {
+  color: white;
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+}
+
+a:hover {
+  color: yellow;
+  font-size: 16em;
+  font-weight: 900;
+  transform: scale(1.1);
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  background-color: salmon;
+  color: white;
+  text-align: center;
+  margin: auto;
+  padding: 2rem;
+  border-radius: 1rem;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+h1 {
+  font-size: 2.5rem;
+  font-weight: 700;
+  margin-bottom: 0.5rem;
+}
+</style>
